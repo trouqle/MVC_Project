be36e379b140344be04b35736c7be8dd73e99859
Orhan Kaya <59208514+trouqle@users.noreply.github.com>
2023-08-17 14:58:34 +0300

    Full Upload Try

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using DragDrop2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DragDrop2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(IFormFile userfile)
+        {
+            string filename = userfile.FileName;
+            filename = Path.GetFileName(filename);
+            
+            string uploadfilepath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Files", filename);
+            var stream = new FileStream(uploadfilepath, FileMode.Create);
+            
+            userfile.CopyToAsync(stream);
+
+            ViewBag.message = "File Uploaded Successfully";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
